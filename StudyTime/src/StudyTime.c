e18b197facb48b49c71b4ff043e67c3dd5262354
Ludusamo <ludusamo@gmail.com>
2015-04-18 16:38:31 -0400

    StudyTime Test

--- StudyTime/src/StudyTime.c
+++ StudyTime/src/StudyTime.c
@@ -1,47 +1,72 @@
 #include <pebble.h>
 
+#define NUM_MENU_SECTIONS 0
+#define MAX_ENTRIES 10
+
 static Window *window;
-static TextLayer *text_layer;
+static MenuLayer *times_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+int numEntries;
+
+static uint16_t getNumSections(MenuLayer *menuLayer, void *data) {
+	return NUM_MENU_SECTIONS;
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+static uint16_t getNumRows(MenuLayer *menu, uint16_t sectionIndex, void *data) {
+	return numEntries + 1;		
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+static int16_t getHeaderHeight(MenuLayer *menu, uint16_t sectionIndex, void *data) {
+	return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static void drawHeader(GContext *ctx, const Layer *cellLayer, uint16_t sectionIndex, void * data) {
+	menu_cell_basic_header_draw(ctx, cellLayer, "HEADER");
+}
+
+static void drawRow(GContext *ctx, const Layer *cellLayer, MenuIndex *cellIndex, void *data) {
+	if (cellIndex->row != 0) {
+		menu_cell_basic_draw(ctx, cellLayer, "Add Item...", "", NULL);
+	} else {
+		char subject[20];
+		persist_read_string(cellIndex->row, subject, sizeof(subject));
+		menu_cell_basic_draw(ctx, cellLayer, subject, "", NULL);
+	}
+}
+
+static void select(MenuLayer *menu, MenuIndex *cellIndex, void *data) {
+
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+  	Layer *window_layer = window_get_root_layer(window);
+  	GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+	times_layer = menu_layer_create(bounds);
+	menu_layer_set_callbacks(times_layer, NULL, (MenuLayerCallbacks) {
+	.get_num_sections = getNumSections,
+	.get_num_rows = getNumRows,
+	.get_header_height = getHeaderHeight,
+	.draw_header = drawHeader,
+	.draw_row = drawRow,
+	.select_click = select,
+	});
+
+	menu_layer_set_click_config_onto_window(times_layer, window);
+	layer_add_child(window_layer, menu_layer_get_layer(times_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+	menu_layer_destroy(times_layer);
 }
 
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
-  const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(window, true);
 }
 
 static void deinit(void) {
